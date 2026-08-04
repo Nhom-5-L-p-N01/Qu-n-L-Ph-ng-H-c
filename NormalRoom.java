6baaea1460b23270543338a12ddd7e4ebb41ef10
chanh206-ai <24100458@st.phenikaa-uni.edu.vn>
2026-08-05 02:41:34 +0700

    Refactor NormalRoom constructor and toString method

--- NormalRoom.java
+++ NormalRoom.java
@@ -1,18 +1,35 @@
 package model;
 
+
 public class NormalRoom extends Room {
 
-    public NormalRoom(String roomId,
-                      String roomName,
-                      int floor,
-                      int capacity,
-                      String status) {
-
-        super(roomId, roomName, floor, capacity, status);
+    // Constructor mặc định
+    public NormalRoom() {
+        super();
     }
 
+    // Constructor đầy đủ
+    public NormalRoom(String roomId, String roomName, int floor,
+                      int capacity, String status) {
+
+        super(roomId, roomName, floor, capacity,
+                "Normal Room", status);
+    }
+
+    
     @Override
     public double calculateFee(int hours) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "NormalRoom{" +
+                "roomId='" + getRoomId() + '\'' +
+                ", roomName='" + getRoomName() + '\'' +
+                ", floor=" + getFloor() +
+                ", capacity=" + getCapacity() +
+                ", status='" + getStatus() + '\'' +
+                '}';
     }
 }

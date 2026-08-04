cb6adaa4f9debd6753f2ea1fb873dba9dc7567d8
chanh206-ai <24100458@st.phenikaa-uni.edu.vn>
2026-08-05 02:39:41 +0700

    Enhance Student class with className and toString method
    Add className property and update constructors for Student class.

--- Student.java
+++ Student.java
@@ -1,24 +1,43 @@
 package model;
 
+/**
+ * Lớp Student kế thừa từ User.
+ * Dùng để quản lý thông tin sinh viên đặt phòng.
+ */
 public class Student extends User {
 
     private String className;
 
+    // Constructor mặc định
     public Student() {
+        super();
     }
 
-    public Student(String id, String fullName, String phone,
+    // Constructor đầy đủ
+    public Student(String id, String fullName, String phoneNumber,
                    String email, String className) {
 
-        super(id, fullName, phone, email);
+        super(id, fullName, phoneNumber, email);
         this.className = className;
     }
 
+    // Getter & Setter
     public String getClassName() {
         return className;
     }
 
     public void setClassName(String className) {
         this.className = className;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + getId() + '\'' +
+                ", fullName='" + getFullName() + '\'' +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", className='" + className + '\'' +
+                '}';
     }
 }

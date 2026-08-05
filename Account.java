364f764dce14fe67efd1cb3ccbcf9117686d2ab4
le533 <24100048@st.phenikaa-uni.edu.vn>
2026-08-05 11:10:45 +0700

    Add student ID and class fields to Account

--- Account.java
+++ Account.java
@@ -3,14 +3,19 @@ public class Account {
     private String email;
     private String soDienThoai;
     private String matKhau;
+    private String maSV;
+    private String lop;
 
     public Account() {}
 
-    public Account(String hoTen, String email, String soDienThoai, String matKhau) {
+    public Account(String hoTen, String email, String soDienThoai, String matKhau,
+                    String maSV, String lop) {
         this.hoTen = hoTen;
         this.email = email;
         this.soDienThoai = soDienThoai;
         this.matKhau = matKhau;
+        this.maSV = maSV;
+        this.lop = lop;
     }
 
     public String getHoTen() { return hoTen; }
@@ -25,14 +30,20 @@ public class Account {
     public String getMatKhau() { return matKhau; }
     public void setMatKhau(String matKhau) { this.matKhau = matKhau; }
 
-    // Chuyển thành 1 dòng để lưu file (ho ten|email|sdt|mat khau)
+    public String getMaSV() { return maSV; }
+    public void setMaSV(String maSV) { this.maSV = maSV; }
+
+    public String getLop() { return lop; }
+    public void setLop(String lop) { this.lop = lop; }
+
+    // Chuyển thành 1 dòng để lưu file (ho ten|email|sdt|mat khau|ma sv|lop)
     public String toLine() {
-        return hoTen + "|" + email + "|" + soDienThoai + "|" + matKhau;
+        return hoTen + "|" + email + "|" + soDienThoai + "|" + matKhau + "|" + maSV + "|" + lop;
     }
 
     public static Account fromLine(String line) {
         String[] p = line.split("\\|", -1);
-        if (p.length < 4) return null;
-        return new Account(p[0], p[1], p[2], p[3]);
+        if (p.length < 6) return null;
+        return new Account(p[0], p[1], p[2], p[3], p[4], p[5]);
     }
 }
